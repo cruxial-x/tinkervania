663e8e7351d21144a48d61cdd83e24c7b78a851c
cruxial-x <seymour.jordan93@gmail.com>
2024-02-16 18:23:25 -0600

    dudes walkin

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Horizontal Movement Settings")]
+    [SerializeField]private float walkSpeed = 1;
+
+    [Header("Jump Settings")]
+    [SerializeField]private float jumpForce = 30;
+    [SerializeField] private Transform groundCheckPoint;
+    [SerializeField] private float groundCheckY = 0.2f;
+    [SerializeField] private float groundCheckX = 0.5f;
+    [SerializeField] private LayerMask whatIsGround;
+
+    private Rigidbody2D player;
+    private float xAxis;
+    Animator animator;
+
+    public static PlayerController Instance;
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputs();
+        Move();
+        Jump();
+        Flip();
+    }
+
+    void GetInputs()
+    {
+        xAxis = Input.GetAxis("Horizontal");
+    }
+    void Flip()
+    {
+        if (xAxis > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (xAxis < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    private void Move()
+    {
+        player.velocity = new Vector2(xAxis * walkSpeed, player.velocity.y);
+        animator.SetBool("Walking", xAxis != 0 && Grounded());
+    }
+
+    public bool Grounded()
+    {
+        if (Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckY, whatIsGround) 
+        || Physics2D.Raycast(groundCheckPoint.position + new Vector3(groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround) 
+        || Physics2D.Raycast(groundCheckPoint.position + new Vector3(-groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void Jump()
+    {
+        // Jump when grounded
+        if (Input.GetButtonDown("Jump") && Grounded())
+        {
+            player.velocity = new Vector3(player.velocity.x, jumpForce);
+        }
+
+        // Half vertical velocity when jump button is released
+        if (Input.GetButtonUp("Jump") && player.velocity.y > 0)
+        {
+            player.velocity = new Vector3(player.velocity.x, player.velocity.y * 0.5f);
+        }
+
+        animator.SetBool("Jumping", !Grounded());
+    }
+}
